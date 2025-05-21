11aad19ce58a2f79120c454f4d888447d7558c57
konra <konrad.czamanski@gmail.com>
2025-05-21 13:28:42 +0200

    Fixes

--- Data/NPCs/joren_halfhand/joren_halfhand.js
+++ Data/NPCs/joren_halfhand/joren_halfhand.js
@@ -1,6 +1,6 @@
 export default {
     name: "Joren Halfhand",
-    portrait: "Media/NPC/joren_halfhand.jpg",
+    portrait: "Media/NPC/joren-halfhand.jpg",
     dialogues: ["base"],
     tradeInventory: ["fogCharm", "healingPotion"]
 };
--- Data/NPCs/sylvara_tidewalker/sylvara_tidewalker.js
+++ Data/NPCs/sylvara_tidewalker/sylvara_tidewalker.js
@@ -1,6 +1,6 @@
 export default {
     name: "Sylvara Tidewalker",
-    portrait: "Media/NPC/sylvara_tidewalker.jpg",
+    portrait: "Media/NPC/sylvara-tidewalker.jpg",
     dialogues: ["base"],
     tradeInventory: ["fogCharm", "healingPotion"]
 };
