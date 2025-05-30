50d7682f23c27587f8219f4ef1226d0592353fb9
konra <konrad.czamanski@gmail.com>
2025-05-30 15:44:43 +0200

    x

--- Data/NPCs/old_maris/old_maris.js
+++ Data/NPCs/old_maris/old_maris.js
@@ -1,7 +1,34 @@
 export default {
-    name: "Mistress Elara",
+    name: "Old Maris",
     portrait: "Media/NPC/old_maris.jpg",
     faction: "unaligned",
+    dialogues: {
+        // Location-based dialogue organization
+        orphansHollow: [
+            {
+                id: "cache_return",
+                conditions: [
+                    {
+                        type: "item",
+                        itemId: "foodSupplies"
+                    }
+                ],
+                priority: 3
+            },
+            {
+                id: "base",
+                conditions: [], // No conditions - this is the default dialogue
+                priority: 1
+            }
+        ],
+        // Default dialogue for any other location
+        default: [
+            {
+                id: "base",
+                conditions: [], // No conditions - this is the default dialogue
+                priority: 0
+            }
+        ]
+    },
     tradeInventory: ["fogCharm", "healingPotion"]
-
-} 
+}; 
